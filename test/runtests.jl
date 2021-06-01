ee3e4fd05e28e5209678fcc8c2a2e424c92cc465
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-06-01 17:01:52 +0000

    Add tests for GenericBlock

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using DPMNeal3
+using Random
 using Test
 
-@testset "DPMNeal3.jl" begin
-    # Write your tests here.
+@testset "GenericBlock.jl" begin
+    rng = MersenneTwister(1)
+    @test_throws AssertionError GenericBlock(rng, 10; K0 = 20)
+    @test_throws AssertionError GenericBlock(rng, 10; K0 = -1)
+    @test_throws AssertionError GenericBlock(rng, 10; a0 = -1)
+    @test_throws AssertionError GenericBlock(rng, 10; b0 = -1)
+    @test_throws TypeError      GenericBlock(rng, 10; K0 = .5)
+    @test_throws MethodError    GenericBlock(rng, .5)
+    N  = 4
+    K0 = 2
+    gb = GenericBlock(rng, N; K0 = K0)
+    @test unique(gb.d) == collect(1:K0)
+    @test length(gb.d) == N
+    @test gb.A == Set(1:K0)
+    @test gb.P == [K0 + 1]
+    @test gb.K[] == K0
 end
