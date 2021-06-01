d41c59519e44eb1394560c165feed87e1f7db4bd
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-06-01 16:39:22 +0000

    Simplify the interface and add a method for updating alpha

--- src/DPMNeal3.jl
+++ src/DPMNeal3.jl
@@ -1,81 +1,92 @@
 module DPMNeal3
 
-using Parameters: @with_kw, @unpack
-using Random: AbstractRNG, randperm!
+# 1. Imports/Exports
 
-@with_kw struct GenericBlock
-    N::Int                        # sample size
-    α::Ref{Float64} = Ref(1.0)    # DP mass parameter
-    τ::Vector{Int} = collect(1:N) # ordering of the observations
-    d::Vector{Int} = ones(N)      # cluster labels
-    n::Vector{Int} = [N]          # cluster sizes
-    P::Vector{Int} = [2]          # passive clusters
-    A::Set{Int} = [1]             # active clusters
-    a0::Float64 = 2               # shape parameter in α's prior
-    b0::Float64 = 4               # rate parameter in α's prior
-end
+using Distributions: Beta, Gamma 
+using Parameters: @unpack
+using Random: randperm!
+export GenericBlock, update!
 
-abstract type AbstractSpecificBlock end
-abstract type AbstractDPMModel end
+# 2. Types
 
-struct DPMModel{SpecificBlock <: AbstractSpecificBlock} <: AbstractDPMModel
-    sb::SpecificBlock
-    gb::GenericBlock
-end
-
-logpdf(m::AbstractDPMModel, y, i, j) = error("Not implemented")
-update_suffstats!(m::AbstractDPMModel, y) = error("not implemented")
-update_suffstats!(m::AbstractDPMModel, y, i, j1, j2) = error("not implemented")
-
-function update!(m::AbstractDPMModel, rng, y)
-    # Compute the sufficient statistics from scratch
-    @unpack A, P, d, n, τ, α = m.gb
-    update_suffstats!(m, y)
-
-    # Update each cluster label
-    for i in randperm!(rng, τ)
-        # Remove one observation from cluster d0
-        d0 = d[i]
-        n[d0] -= 1
-        if n[d0] == 0
-            pop!(A, d0)
-            push!(P, d0)
-        end
-
-        # Update d[i]
-        d1 = P[end]
-        p1 = logpdf(m, y, i, d1) + log(α[]) - log(-log(rand(rng)))
-        for k in A
-            p = logpdf1(m, y, i, k) + log(n[k]) - log(-log(rand(rng)))
-            p < p1 && continue
-            d1 = k
-            p1 = p
-        end
-        d[i] = d1
-
-        # Resize n if necessary
-        if length(n) < d1
-            push!(n, 0)
-        end
-
-        # Add one observation to cluster d1
-        if n[d1] == 0
-            push!(A, d1)
-            last_popped = pop!(P, d1)
-            isempty(P) && push!(P, last_popped + 1)
-        end
-        n[d1] += 1
-
-        # Update the sufficient statistics as d[i] changes
-        update_suffstats!(m, y, i, d0, d1)
+struct GenericBlock
+    N::Int          # sample size
+    K::Ref{Int}     # number of clusters
+    α::Ref{Float64} # DP mass parameter
+    τ::Vector{Int}  # ordering of the observations
+    d::Vector{Int}  # cluster labels
+    n::Vector{Int}  # cluster sizes
+    P::Vector{Int}  # passive clusters
+    A::Set{Int}     # active clusters
+    a0::Float64     # shape parameter in p(α)
+    b0::Float64     # rate parameter in p(α)
+    function GenericBlock(rng, N; K0 = 8, a0 = 2, b0 = 4)
+        @assert N >= K0
+        K = Ref(K0)
+        α = Ref(1.0)
+        τ = randperm(rng, N)
+        d = rand(rng, 1:K0, N); d[1:K0] = 1:K0;
+        n = [sum(d .== k) for k in 1:(K0 + 1)]
+        P = [K0 + 1]
+        A = Set(1:K0)
+        new(N, K, α, τ, d, n, P, A, a0, b0)
     end
 end
 
+# 3. Interface 
+# Any DPM specific block (sb) must implement these functions
+
+function predloglik(sb, gb::GenericBlock, data, i, k)
+    # Return log p(y[i] | y[-i], d[-i], d[i] = k)
+    error("not implemented")
+end
+
+function update_suffstats!(sb, gb::GenericBlock, data)
+    # Recompute the sufficient statistics from scratch
+    error("not implemented")
+end
+
+function update_suffstats!(sb, gb::GenericBlock, data, i, k0, k1)
+    # Recompute the sufficient statistics after `d[i]` changes from `k0` to `k1`
+    error("not implemented")
+end
+
+# 4. Gibbs update logic
+
+function update!(rng, sb, gb::GenericBlock, data)
+    update_d!(rng, sb, gb, data)
+    update_α!(rng, gb)
+end
+
+function update_d!(rng, sb, gb::GenericBlock, data)
+    @unpack A, P, d, n, τ, α = gb
+    update_suffstats!(sb, gb, data)
+
+    for i in randperm!(rng, τ)
+        d0 = d[i]
+        d1 = P[end]
+        p1 = predloglik(sb, gb, data, i, d1) + log(α[]) - log(-log(rand(rng)))
+        for k in A
+            p = predloglik(sb, gb, data, i, k) 
+            p += log(n[k] - (d0 == k))
+            p -= log(-log(rand(rng)))
+            p > p1 && (d1 = k; p1 = p)
+        end
+        if d1 != d0
+            update_suffstats!(sb, gb, data, i, d0, d1)
+            (n[d0] -= 1) == 0 && (push!(P, d0); pop!(A, d0); K[] -= 1)
+            (n[d1] += 1) == 1 && (push!(A, d1); pop!(P, d1); K[] += 1)
+            isempty(P) && (push!(n, 0); push!(P, K[] + 1))
+            d[i] = d1
+        end
+    end
+end
+
+function update_α!(rng, gb::GenericBlock)
+    @unpack N, K, α, a0, b0 = gb
+    ϕ = rand(rng, Beta(α[] + 1.0, N))
+    ψ = 1.0 / (1.0 + N * (b0 - log(ϕ)) / (a0 + M[] - 1.0))
+    α[] = rand(rng, Gamma(a0 + K[] - (rand(rng) > ψ), 1.0 / (b0 - log(ϕ))))
+end
+
 end # module
-
-# N(m::AbstractDPMModel) = m.gb.N
-# d(m::AbstractDPMModel) = m.gb.d
-# n(m::AbstractDPMModel) = m.gb.n
-# D(m::AbstractDPMModel) = m.gb.D
-# α(m::AbstractDPMModel) = m.gb.α
-
