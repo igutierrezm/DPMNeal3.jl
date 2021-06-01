220c3f3aa4caef7afea901b00d6f2a3071554625
Documenter.jl <documenter@juliadocs.github.io>
2021-06-01 00:15:51 +0000

    build based on e259d1d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DPMNeal3","category":"page"},{"location":"#DPMNeal3","page":"Home","title":"DPMNeal3","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DPMNeal3.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DPMNeal3]","category":"page"}]
+}
